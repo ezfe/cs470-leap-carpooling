a5dfebd4fd32791c25d2e23b52f994cf20eb0328
Ezekiel Elin <ezekielelin@me.com>
2020-02-13 10:44:29 -0500

    Setup basic postgres usage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const { Pool } = require('pg')
+require('dotenv').config()
+
+/* Postgres Setup */
+const pool = new Pool()
+
+/* Express Setup */
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  const client = await pool.connect()
+  try {
+    const response = await client.query('SELECT NOW()')
+    res.send(JSON.stringify(response.rows[0]))
+  } finally {
+    client.release()
+  }
 })
 
 app.listen(8000, () => {
