bd3e59cfe3ffcb1cebf1cf7aec945d85877c5f5c
Ezekiel Elin <ezekielelin@me.com>
2020-02-22 22:32:01 -0500

    Fixed the knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
+      host: (process.env.PGHOST || 'localhost'),
+      user: (process.env.PGUSER || 'carpool'),
+      password: (process.env.PGPASSWORD || 'carpooldb'),
       database: (process.env.PGDATABASE || 'carpooldb')
     },
     migrations: {
