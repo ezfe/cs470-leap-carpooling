c488dc3044ba1515af16b210378db351f5d23cee
Ezekiel Elin <ezekielelin@me.com>
2020-03-07 12:52:08 -0500

    Added sessions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const routes = require('./routes')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 
 /* Load environment variables from .env file */
 dotenv.config()
@@ -9,6 +10,12 @@ dotenv.config()
 /* Express Setup */
 const app = express()
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000 }
+}))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.set('view engine', 'pug')
