a2715df8996bd6f3135d5e71e31f57eba90e1988
Nicole Kaplan <kaplann@lafayette.edu>
2020-03-08 16:29:56 -0400

    implemented user onboarding form submission

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,31 +15,25 @@ routes.use('/trips', dashboard)
 routes.post('/onboard', requireAuthenticated, async(req, res) => {
   try {
     // currentUser should be guarunteed when requireAuthenticated is run
-    const currentUser = req.user
+    const currentUser = req.user.id
+    console.error(currentUser)
+    const records = await db('users')
+      .where('id', '=', currentUser)
+      .update({
+        preferred_name: req.body.preferred_name,
+        email: req.body.preferred_email,
+        phone_number: req.body._phone
+       })
 
-    // const records = await db('users')
-    //   .returning('*')
-    //   .where('id', '=', currentUser)
-    //   .update({
-    //     preferred_name: req.body.preferred_name,
-    //     email: req.body.preferred_email,
-    //     phone_number: req.body._phone
-    //   })
-    // res.redirect('/')
-
-    res.send('Unfinished')
-    // res.render('onboard')
+      res.redirect('/')
   } catch (err) {
+    console.error(err)
     res.render('database-error')
   }
 })
 
 routes.get('/onboard', (req, res) => {
   res.render('onboard')
-  // const prefName = preferred_name
-  // const preEmail = preferred_email
-  // const phone =  _phone
-
   res.send('Unfinished')
 })
 
