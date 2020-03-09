6cb4a4bf9a511413e5284031c6033f957c105202
Garrett Rademacher <rademacher.garrett@gmail.com>
2020-03-09 15:10:17 -0400

    started adding ID's/db code to add new trip requests

--- src/routes/trip_creation.js
+++ src/routes/trip_creation.js
@@ -1,8 +1,27 @@
 const routes = require('express').Router()
+const db = require('../db')
 
 // /trips/new
 routes.get('/', (req, res) => {
   res.render('new_trip')
+})
+
+// POST /sessions/create-user
+routes.post('/create-user', async (req, res) => {
+  try {
+    await db('trip_requests').insert({
+      id: req.body.netid,
+      member_id: req.body.first_name,
+      role: req.body.last_name,
+      location: req.body.location,
+      deviation_limit: req.body.deviation_limit,
+      direction: req.body.direction,
+      created_at: db.fn.now()
+    })
+    res.redirect('/')
+  } catch (err) {
+    res.render('database-error')
+  }
 })
 
 module.exports = routes
