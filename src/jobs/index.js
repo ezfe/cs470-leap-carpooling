daef521be4fdfc667b912adce39ea814d35893e8
Ezekiel Elin <ezekielelin@me.com>
2020-03-11 15:59:56 -0400

    Enabled the sample job

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -3,7 +3,7 @@ const schedule = require('node-schedule')
 const sampleJob = require('./sample-job')
 
 function registerJobs() {
-  // schedule.scheduleJob('*/5 * * * * *', sampleJob)
+  schedule.scheduleJob('*/5 * * * * *', sampleJob)
 }
 
 module.exports = registerJobs
