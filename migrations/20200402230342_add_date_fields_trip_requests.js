c8e8d0f1f9b7f73e36630e94ec536fea3617c622
Ezekiel Elin <ezekielelin@me.com>
2020-04-03 11:47:45 -0400

    Corrected issues with migrations and merging

---
+++ migrations/20200402230342_add_date_fields_trip_requests.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.table('trip_requests', table => {
+    table.date('start_date')
+    table.date('end_date')
+
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.table('trip_requests', table => {
+    table.dropColumn('start_date')
+    table.dropColumn('end_date')
+
+  })
+};
