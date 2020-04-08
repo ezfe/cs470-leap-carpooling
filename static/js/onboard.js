34ea22bfc4baec09b19d682226919deb0932af39
Nicole Kaplan <nkap1234@gmail.com>
2020-04-07 22:45:14 -0400

    Added form validation to the onboarding page

--- static/js/onboard.js
+++ static/js/onboard.js
@@ -7,3 +7,62 @@ function displayPhoto(event) {
 // Can do JS client-side validation here in the
 // future
 // })
+
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return true
+  }
+    return false
+}
+
+function ValidatePhone(phone)
+{
+  var phoneno = /^\d{10}$/;
+  if(phoneno.test(phone))
+  {
+    return true
+  }
+  else
+  {
+    return false
+  }
+}
+function ValidateName(name)
+{
+  var na = /^[A-Za-z]+$/
+  if(na.test(name))
+  {
+    return true
+  }
+  else
+  {
+    return false
+  }
+}
+
+document.getElementById('onboard_form').addEventListener('submit', (event) => {
+  const email = document.getElementById("preferred_email").value
+
+  if(ValidateEmail(email)===false)
+  {
+    event.preventDefault()
+    alert("Please enter a valid email address")
+    return
+  }
+  const phone = document.getElementById("phone_number").value
+  if(ValidatePhone(phone)===false)
+  {
+    event.preventDefault()
+    alert("Please enter a valid 10 digit phone number")
+    return
+  }
+  const name = document.getElementById("preferred_name").value
+  if(ValidateName(name)===false)
+  {
+    event.preventDefault()
+    alert("Please enter a valid name")
+    return
+  }
+})
