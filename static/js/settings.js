753a978df65fa43ea5a913fe4376bbc4d880b300
Nicole Kaplan <nkap1234@gmail.com>
2020-04-09 18:39:41 -0400

    Started form validation on settings page

--- static/js/settings.js
+++ static/js/settings.js
@@ -12,8 +12,32 @@ const formSync = registerAutocomplete('location_field', 'place_id_field')
 
 document.getElementById('request_form').addEventListener('submit', (event) => {
   if (!formSync()) {
-    alert("No Place ID!")
+    alert("Please enter a valid location")
     event.preventDefault()
+    return
+  }
+  const deviation_limit = document.getElementById('deviation_limit').value
+  if(deviation_limit == ""){
+    alert('Please enter a deviation limit between 0 minutes and 120 minutes')
+    event.preventDefault()
+    return
+  }
+  if(deviation_limit > 1440){
+    alert('Please choose a max deviation limit below 1440 minutes')
+    event.preventDefault()
+    return
+    }
+  if(isNaN(deviation_limit)){
+    alert('Please enter a valid deviation limit between 0 minutes and 120 minutes')
+    event.preventDefault()
+    return
+  }
+  const phone = document.getElementById("_phone").value
+  var na = /^\d{10}$/;
+  if(!na.test(phone))
+  {
+    event.preventDefault()
+    alert("Please enter a valid 10 digit phone number")
     return
   }
 })
