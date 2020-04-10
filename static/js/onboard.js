a369ca33ad694076746d6bd339b7ddfbe7c072ce
Ezekiel Elin <ezekielelin@me.com>
2020-04-10 09:10:49 -0400

    Cleaned up validation code a little bit

--- static/js/onboard.js
+++ static/js/onboard.js
@@ -3,64 +3,26 @@ function displayPhoto(event) {
   image.src = URL.createObjectURL(event.target.files[0])
 }
 
-// document.getElementById('onboard-form').addEventListener('submit', (event) => {
-// Can do JS client-side validation here in the
-// future
-// })
-
-function ValidateEmail(mail) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
-  {
-    return true
-  }
-    return false
+function validatePhone(phone) {
+  const phoneno = /^\d{10}$/;
+  return phoneno.test(phone)
 }
 
-function ValidatePhone(phone)
-{
-  var phoneno = /^\d{10}$/;
-  if(phoneno.test(phone))
-  {
-    return true
-  }
-  else
-  {
-    return false
-  }
-}
-function ValidateName(name)
-{
-  var na = /^[A-Za-z]+$/
-  if(na.test(name))
-  {
-    return true
-  }
-  else
-  {
-    return false
-  }
+function validateName(name) {
+  const na = /^[A-Za-z]+$/
+  return na.test(name)
 }
 
 document.getElementById('onboard_form').addEventListener('submit', (event) => {
-  const email = document.getElementById("preferred_email").value
-
-  if(ValidateEmail(email)===false)
-  {
-    event.preventDefault()
-    alert("Please enter a valid email address")
-    return
-  }
   const phone = document.getElementById("phone_number").value
-  if(ValidatePhone(phone)===false)
-  {
+  if(validatePhone(phone) === false) {
     event.preventDefault()
     alert("Please enter a valid 10 digit phone number")
     return
   }
+
   const name = document.getElementById("preferred_name").value
-  if(ValidateName(name)===false)
-  {
+  if(validateName(name) === false) {
     event.preventDefault()
     alert("Please enter a valid name")
     return
