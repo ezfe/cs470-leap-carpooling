4bb420f57223ad671726aefee11af142aba4742f
Ezekiel Elin <ezekielelin@me.com>
2020-04-10 14:04:36 -0400

    Better onboarding validation style

--- static/js/onboard.js
+++ static/js/onboard.js
@@ -3,28 +3,11 @@ function displayPhoto(event) {
   image.src = URL.createObjectURL(event.target.files[0])
 }
 
-function validatePhone(phone) {
-  const phoneno = /^\d{10}$/;
-  return phoneno.test(phone)
-}
-
-function validateName(name) {
-  const na = /^[A-Za-z]+$/
-  return na.test(name)
-}
-
 document.getElementById('onboard_form').addEventListener('submit', (event) => {
-  const phone = document.getElementById("phone_number").value
-  if(validatePhone(phone) === false) {
+  if (!event.target.checkValidity()) {
     event.preventDefault()
-    alert("Please enter a valid 10 digit phone number")
-    return
+    event.stopPropagation()
+    alert("Please correct the highlighted errors")
   }
-
-  const name = document.getElementById("preferred_name").value
-  if(validateName(name) === false) {
-    event.preventDefault()
-    alert("Please enter a valid name")
-    return
-  }
+  event.target.classList.add('was-validated')
 })
