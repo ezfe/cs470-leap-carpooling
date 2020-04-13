6af160efdec244f61f8b8d3436257865a70071e3
Ezekiel Elin <ezekielelin@me.com>
2020-04-13 10:13:48 -0400

    Removed initMap

--- static/js/trip_detail.js
+++ static/js/trip_detail.js
@@ -16,37 +16,35 @@ document.getElementById('cancel-trip-form').addEventListener('submit', (evt) => 
     }
 })
 
-function initMap() {
-  let directionsService = new google.maps.DirectionsService
-  let directionsRenderer = new google.maps.DirectionsRenderer
-  let map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6,
-    center: {lat: 41.85, lng: -87.65}
-  })
-  directionsRenderer.setMap(map)
+let directionsService = new google.maps.DirectionsService
+let directionsRenderer = new google.maps.DirectionsRenderer
+let map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 6,
+  center: {lat: 41.85, lng: -87.65}
+})
+directionsRenderer.setMap(map)
 
-  const firstPlaceID = getMeta('firstPlaceID')
-  const lastPlaceID = getMeta('lastPlaceID')
-  const midPlaceID = getMeta('midPlaceID')
+const firstPlaceID = getMeta('firstPlaceID')
+const lastPlaceID = getMeta('lastPlaceID')
+const midPlaceID = getMeta('midPlaceID')
 
-  let options = {
-    origin: { placeId: firstPlaceID },
-    destination: { placeId: lastPlaceID },
-    optimizeWaypoints: false,
-    travelMode: 'DRIVING'
-  }
-
-  if (midPlaceID != lastPlaceID && midPlaceID != firstPlaceID) {
-    options.waypoints = { location: { placeId: getMeta('midPlaceID') }}
-  }
-
-  console.log(options)
-
-  directionsService.route(options, (response, status) => {
-    if (status === 'OK') {
-      directionsRenderer.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  })
+let options = {
+  origin: { placeId: firstPlaceID },
+  destination: { placeId: lastPlaceID },
+  optimizeWaypoints: false,
+  travelMode: 'DRIVING'
 }
+
+if (midPlaceID != lastPlaceID && midPlaceID != firstPlaceID) {
+  options.waypoints = { location: { placeId: getMeta('midPlaceID') }}
+}
+
+console.log(options)
+
+directionsService.route(options, (response, status) => {
+  if (status === 'OK') {
+    directionsRenderer.setDirections(response);
+  } else {
+    window.alert('Directions request failed due to ' + status);
+  }
+})
