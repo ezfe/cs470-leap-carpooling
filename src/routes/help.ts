a00b49c4b6f7f1a4e04b07427d9e3d464a8d797b
Natalie <sampseln@lafayette.edu>
2020-04-21 15:20:01 -0400

    Add initial email form

--- src/routes/help.ts
+++ src/routes/help.ts
@@ -1,10 +1,16 @@
 import { Response, Router } from 'express'
 import { AuthedReq } from '../utils/authed_req'
+import { preferredEmail } from '../validation'
 
 const routes = Router()
 
 routes.get('/', async (req: AuthedReq, res: Response) => {
-  res.render('help')
+  const userEmail = (req.user) ? req.user.email : ''
+  res.render('help', { userEmail, preferredEmail })
+})
+
+routes.post('/', async (req: AuthedReq, res: Response) => {
+  res.redirect('/help')
 })
 
 export default routes
