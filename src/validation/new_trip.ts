ab20918f31ca6abbeb22010a0c45413b557af2c3
Ezekiel Elin <ezekielelin@me.com>
2020-04-26 17:31:46 -0400

    Fixed validation typo

--- src/validation/new_trip.ts
+++ src/validation/new_trip.ts
@@ -5,7 +5,7 @@ import {} from '.'
 export const newTripSchema = Joi.object({
   user_role: Joi.string().valid('driver', 'rider').required(),
   place_id: Joi.string().required(),
-  trip_direciton: Joi.string().valid('towards_lafayette', 'from_lafayette').required(),
+  trip_direction: Joi.string().valid('towards_lafayette', 'from_lafayette').required(),
   first_date: Joi.string().required(),
   last_date: Joi.string().required(),
 
