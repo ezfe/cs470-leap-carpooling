7f027f21378c87a94c8ab52ee061fe31b5b658d4
Ezekiel Elin <ezekielelin@me.com>
2020-05-04 13:11:47 -0400

    Updated start script

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
-cd /home/carpool/cs470-leap-carpooling
+#!/bin/bash
+
+# This file will ensure your database is migrated and then
+# build and run the server. It should be run consistently
+# from the same user, since it will result in file system
+# changes. It is designed to go hand-in-hand with the
+# systemd service provided in `services/carpool.service`.
 
 yarn
 yarn migrate:latest
-yarn tsc
-yarn run node
+yarn run start
